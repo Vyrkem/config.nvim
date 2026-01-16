02d19b3ee8e2b802df3d018b0a3c99c6d24b694c
ced.cm <ced.bazyk@gmail.com>
2026-01-16 13:50:24 -0500

    modif

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -11,7 +11,19 @@ return {
         start_in_insert = true,
         insert_mappings = true,
         persist_size = true,
+        shading_factor = 2,
+        persist_mode = true,
+        close_on_exit = true,
+        auto_scroll = true,
+        direction = 'horizontal', -- optionnel, mais pratique
       }
+      vim.api.nvim_create_autocmd('TermOpen', {
+        pattern = '*',
+        callback = function()
+          -- En mode terminal, Esc = <C-\><C-n>
+          vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { buffer = true, silent = true })
+        end,
+      })
     end,
   },
 }
